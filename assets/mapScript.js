4b4b9985f53f9255f7c0af15df82d3ff9709692a
itelman <84926411+itelman@users.noreply.github.com>
2024-08-01 17:28:39 +0500

    Update mapScript.js

--- assets/mapScript.js
+++ assets/mapScript.js
@@ -1,11 +1,10 @@
-const apiKey = process.env.GOOGLE_API_KEY;
-
 function myMap() {
     var mapProp = {
         center: new google.maps.LatLng(51.508742, -0.120850),
         zoom: 1,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var apiKey = document.getElementById("api_key");
 
     let locations = document.getElementsByClassName('left');
     var infowindow = new google.maps.InfoWindow();
